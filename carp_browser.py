c8254cd5c9b9135e59353c5a11bb26654344ce6b
neavemj <matthewjneave1@gmail.com>
2016-04-19 14:43:14 +1000

    add text widget

--- carp_browser.py
+++ carp_browser.py
@@ -15,6 +15,7 @@ class carp_browser:
         self.root = root
         root.configure(background="#333")
         root.title(" Carp Genome Browser ")
+        
         # self.root.iconbitmap('newicon') # change little TK icon     
         
         ## initialize GUI ##
@@ -32,26 +33,57 @@ class carp_browser:
         self.menu_bar.add_cascade(label="Edit", menu=self.edit_menu)
         root.config(menu=self.menu_bar)        
         
-        # add left option pane
-        ttk.Style().configure("TButton", background="#333")
+        #### add left option pane ####
+
+        self.left_pane = Tkinter.Frame(root, height=800, width=300, bg="#333",
+                                       bd=1, relief=Tkinter.SUNKEN)
+        self.left_pane.grid(row=0, column=0, padx=20, pady=20, sticky="n")        
+                
+        ttk.Style().configure("TCheckbutton", background="#333", foreground="gray",
+                        takefocus=False)     
+           
+        self.chk1 = ttk.Checkbutton(self.left_pane, text="Cyprinus carpio")                               
+        self.chk1.grid(row=0, column=0, padx=20, pady=10, sticky="w")
+        self.chk2 = ttk.Checkbutton(self.left_pane, text="Danio Rerio")                               
+        self.chk2.grid(row=1, column=0, padx=20, pady=10, sticky="w")
+        self.chk3 = ttk.Checkbutton(self.left_pane, text="Tilapia")                               
+        self.chk3.grid(row=2, column=0, padx=20, pady=10, sticky="w")
+        self.chk4 = ttk.Checkbutton(self.left_pane, text="CyHV-3")                               
+        self.chk4.grid(row=3, column=0, padx=20, pady=10, sticky="w")        
+
+        #### add middle search screens ####
+
+        self.middle_pane = Tkinter.Frame(root, height=800, width=300, bg="#333",
+                                         bd=1, relief=Tkinter.SUNKEN)
+        self.middle_pane.grid(row=0, column=1, padx=20, pady=20) 
         
-        self.b1 = ttk.Button(root, text="b1", style="TButton", 
-                        command=self.but_test)
-        self.b1.grid(row=0, column=0)
-        self.b2 = ttk.Button(root, text="b2")
-        self.b2.grid(row=1, column=0)
-        self.b3 = ttk.Button(root, text="b3")
-        self.b3.grid(row=2, column=0)
-        self.b4 = ttk.Button(root, text="b4")
-        self.b4.grid(row=4, column=1)
+        # load swiss annotations and update text accordingly
         
-    def but_test(self):
-        print "button pressed"
+        swiss_file = open("./data/testAssembly.fasta")
+        #swiss_data = swiss_file.read()
+        #swiss_file.close()
+        
+
+        self.swiss_text = Tkinter.Text(self.middle_pane, width=60, height=10)
+        self.swiss_text.insert(1.0, swiss_file.read())        
+        
+        self.swiss_text.grid(row=0, column=0, padx=20, pady=10, 
+                              sticky=Tkinter.N+Tkinter.E)
+        
+        # load pfam annotations              
+        self.pfam_text = Tkinter.Text(self.middle_pane, width=60, height=10)
+        self.pfam_text.grid(row=1, column=0, padx=20, pady=10,
+                              sticky=Tkinter.S+Tkinter.E)
+                             
+                        
+
+
+
 
 
 if __name__ == "__main__":
     root = Tkinter.Tk()
-    root.geometry("+100+100")
+    root.geometry("1200x800+100+100")
     app = carp_browser(root)
     root.mainloop()
 
