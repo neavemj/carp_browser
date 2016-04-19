f6fbe65534f3d708da8a1b6970ac6e4f6e8d0b0a
neavemj <matthewjneave1@gmail.com>
2016-04-19 16:36:18 +1000

    updated text widgets and checkboxes

--- carp_browser.py
+++ carp_browser.py
@@ -39,47 +39,60 @@ class carp_browser:
                                        bd=1, relief=Tkinter.SUNKEN)
         self.left_pane.grid(row=0, column=0, padx=20, pady=20, sticky="n")        
                 
-        ttk.Style().configure("TCheckbutton", background="#333", foreground="gray",
-                        takefocus=False)     
-           
-        self.chk1 = ttk.Checkbutton(self.left_pane, text="Cyprinus carpio")                               
+        self.chk1 = Tkinter.Checkbutton(self.left_pane, text="Cyprinus carpio",
+                                        bg="#333", fg="#efef8f", selectcolor="#3f3f3f", 
+                                        activebackground="#333")                               
         self.chk1.grid(row=0, column=0, padx=20, pady=10, sticky="w")
-        self.chk2 = ttk.Checkbutton(self.left_pane, text="Danio Rerio")                               
+        
+        self.chk2 = Tkinter.Checkbutton(self.left_pane, text="Danio Rerio",
+                                        bg="#333", fg="#efef8f", selectcolor="#3f3f3f", 
+                                        activebackground="#333")                               
         self.chk2.grid(row=1, column=0, padx=20, pady=10, sticky="w")
-        self.chk3 = ttk.Checkbutton(self.left_pane, text="Tilapia")                               
+        self.chk3 = Tkinter.Checkbutton(self.left_pane, text="Tilapia",
+                                        bg="#333", fg="#efef8f", selectcolor="#3f3f3f", 
+                                        activebackground="#333")                               
         self.chk3.grid(row=2, column=0, padx=20, pady=10, sticky="w")
-        self.chk4 = ttk.Checkbutton(self.left_pane, text="CyHV-3")                               
+        self.chk4 = Tkinter.Checkbutton(self.left_pane, text="CyHV-3",
+                                        bg="#333", fg="#efef8f", selectcolor="#3f3f3f", 
+                                        activebackground="#333")                               
         self.chk4.grid(row=3, column=0, padx=20, pady=10, sticky="w")        
 
         #### add middle search screens ####
 
-        self.middle_pane = Tkinter.Frame(root, height=800, width=300, bg="#333",
+        self.middle_pane = Tkinter.Frame(root, height=800, width=400, bg="#333",
                                          bd=1, relief=Tkinter.SUNKEN)
         self.middle_pane.grid(row=0, column=1, padx=20, pady=20) 
         
-        # load swiss annotations and update text accordingly
-        
-        swiss_file = open("./data/testAssembly.fasta")
-        #swiss_data = swiss_file.read()
-        #swiss_file.close()
-        
-
-        self.swiss_text = Tkinter.Text(self.middle_pane, width=60, height=10)
-        self.swiss_text.insert(1.0, swiss_file.read())        
-        
+        # create swiss text box
+        self.swiss_text = Tkinter.Text(self.middle_pane, width=120, height=20,
+                                       font=("Consolas", 8), padx=5, pady=5, spacing1=5,
+                                        bg="#3f3f3f", fg="#dcdccc")         
         self.swiss_text.grid(row=0, column=0, padx=20, pady=10, 
-                              sticky=Tkinter.N+Tkinter.E)
+                              sticky=Tkinter.N+Tkinter.E+Tkinter.W)
+                 
+
+        self.load_swiss()
         
-        # load pfam annotations              
-        self.pfam_text = Tkinter.Text(self.middle_pane, width=60, height=10)
-        self.pfam_text.grid(row=1, column=0, padx=20, pady=10,
-                              sticky=Tkinter.S+Tkinter.E)
-                             
-                        
-
-
-
-
+    def load_swiss(self):
+        
+        # load swiss annotations and update text accordingly
+    
+        swiss_file = open("./data/swiss.genome_browser")
+        swiss_data = swiss_file.read()
+        self.swiss_text.insert(1.0, swiss_data) 
+        
+        # add color to first column
+        self.swiss_text.tag_configure("id_column", foreground="#cc9393")
+#        swiss_lines = int(self.swiss_text.index("end-1c").split(".")[0])
+#        for lines in range(swiss_lines):
+#            colour_start = str(lines) + ".0"
+#            colour_end = str(lines) + ".11"
+#            self.swiss_text.tag_add("id_column", colour_start, colour_end)
+        for index, lines in enumerate(swiss_data):
+             colour_start = str(index) + ".0"
+             colour_end = str(index) + ".11"
+             self.swiss_text.tag_add("id_column", colour_start, colour_end)
+ 
 
 if __name__ == "__main__":
     root = Tkinter.Tk()
